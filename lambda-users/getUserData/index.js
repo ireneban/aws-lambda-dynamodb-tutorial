8ebfff468ce5537976738a80d04cb2b43055aae0
Mijeong Ban <mban1@stevens.edu>
2019-09-21 18:06:34 -0400

    done with part2

--- lambda-users/getUserData/index.js
+++ lambda-users/getUserData/index.js
@@ -11,18 +11,35 @@ exports.handler = async (event, context) => {
     region: "us-east-1"
   });
 
+  let responseBody = "";
+  let statusCode = 0;
+
+  const { id } = event.pathParameters;
+
   // Describe the item that we want to pull out from DynamoDB
   const params = {
     TableName: "Users",
     Key: {
-      id: "12345"
+      id: id
     }
   };
 
   try {
     const data = await documentClient.get(params).promise();
-    console.log(data);
+    responseBody = JSON.stringify(data.Item);
+    statusCode = 200;
   } catch (err) {
-    console.log(err);
+    responseBody = `Unable to get user data`;
+    statusCode = 403;
   }
+
+  const response = {
+    statusCode: statusCode,
+    headers: {
+      myHeader: "test"
+    },
+    body: responseBody
+  };
+
+  return response;
 };
